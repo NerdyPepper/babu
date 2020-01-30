272715f189614daeb42df6c73cdb1fdcdae82cdf
Akshay <nerdypepper@tuta.io>
2020-01-30 21:39:18 +0530

    kinda like clang format ngl

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,69 @@
 import java.util.Arrays;
-import java.util.stream.Stream;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main (String args[]) {
-        ArgParse parser = new ArgParse(args);
+    public static void main( String args[] ) {
+        ArgParse parser = new ArgParse( args );
     }
 }
 
 class ArgParse {
-    private enum Target {
-        NEW,
-        BUILD,
-        RUN,
-        HELP
-    };
+    private enum Target { NEW, BUILD, RUN, HELP }
+    ;
 
     private Target subCmd;
     private String value;
 
-    private static Target matchTarget (String tar) {
-        switch (tar.toLowerCase()) {
-            case "new":
-            case "n":
-                return Target.NEW;
-            case "build":
-            case "b":
-                return Target.BUILD;
-            case "run":
-            case "r":
-                return Target.RUN;
+    private static Target matchTarget( String tar ) {
+        switch ( tar.toLowerCase() ) {
+        case "new":
+        case "n":
+            return Target.NEW;
+        case "build":
+        case "b":
+            return Target.BUILD;
+        case "run":
+        case "r":
+            return Target.RUN;
         }
         return Target.HELP;
     }
 
-    private void useDefaults () {
-        switch (this.subCmd) {
-            case NEW:
-                this.value = "new_babu_project";
-                break;
+    private void useDefaults() {
+        switch ( this.subCmd ) {
+        case NEW:
+            this.value = "new_babu_project";
+            break;
 
-            default:
-            case BUILD:
-            case RUN:
-                this.value = null;
-                break;
+        default:
+        case BUILD:
+        case RUN:
+            this.value = null;
+            break;
         }
     }
 
-    private static void printHelp () {
-        System.out.println("help text");
-    }
+    private static void printHelp() { System.out.println( "help text" ); }
 
-    public ArgParse (String args[]) {
-        if (args.length == 0) {
+    public ArgParse( String args[] ) {
+        if ( args.length == 0 ) {
             printHelp();
-            System.exit(0);
+            System.exit( 0 );
         } else {
-            this.subCmd = matchTarget(args[0]);
+            this.subCmd = matchTarget( args[ 0 ] );
             try {
-                this.value = Arrays.stream(args)
-                    .skip(1)
-                    .collect(Collectors.joining("_"));
-            } catch (ArrayIndexOutOfBoundsException e) {
+                this.value = Arrays.stream( args ).skip( 1 ).collect(
+                    Collectors.joining( "_" ) );
+            } catch ( ArrayIndexOutOfBoundsException e ) {
                 this.useDefaults();
             }
         }
     }
 
-    public String toString () {
-        String s = String.format("Sub Command: %s, Value: %s",
-                this.subCmd.toString(),
-                this.value);
+    public String toString() {
+        String s = String.format( "Sub Command: %s, Value: %s",
+                                  this.subCmd.toString(), this.value );
         return s;
     }
-
 }
