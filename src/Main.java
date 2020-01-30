890c5750c34f0cfe6acae9b367c74fdcdf15ebba
Akshay <nerdypepper@tuta.io>
2020-01-30 22:52:53 +0530

    up and runnin

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import src.ArgParse;
+import src.Command;
 
 public class Main {
     public static void main( String args[] ) {
         ArgParse parser = new ArgParse( args );
         System.out.println(parser);
+        Command.exec(parser);
     }
 }
 
